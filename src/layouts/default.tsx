7f36747a789a84d76bc412f67092b71ce997ae98
unknown <aramayowil>
2025-08-15 18:58:25 -0300

    fix pdf

--- src/layouts/default.tsx
+++ src/layouts/default.tsx
@@ -1,6 +1,5 @@
 import Footer from '@/components/Footer'
 import { Navbar } from '@/components/navbar'
-import App from '@/components/Prueba'
 
 export default function DefaultLayout({
   children,
@@ -9,7 +8,6 @@ export default function DefaultLayout({
 }) {
   return (
     <div className='grid min-h-dvh grid-rows-[auto_1fr_auto]'>
-      <App />
       <Navbar />
       <main className='container mx-auto max-w-7xl flex-grow px-0 pt-0 md:pt-4 md:px-4 lg:pt-6 lg:px-6'>
         {children}
