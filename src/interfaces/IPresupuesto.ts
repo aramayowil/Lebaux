1dbbfaf78d37a03d69c25cc4ba5a87c767e5a66f
aramayowil <aramayowil@gmail.com>
2026-01-11 13:44:57 -0300

    Ipresupuestos Agregado

---
+++ src/interfaces/IPresupuesto.ts
@@ -0,0 +1,13 @@
+import { IAbertura } from './IAbertura'
+import { IAbertura_Compuesta } from './IAberturaCompuesta'
+
+export interface IPresupuesto {
+  id: string
+  cliente: string
+  fecha: string
+  total: number
+  observaciones?: string 
+  descuento?: number 
+  estado?: 'pendiente' | 'aceptado' | 'rechazado' 
+  items: (IAbertura | IAbertura_Compuesta)[]
+}
--- src/interfaces/Imodulo.ts
+++ src/interfaces/Imodulo.ts
@@ -1,12 +1,9 @@
-// interfaces/IConfiguracionModulo.ts
 import { IAbertura } from './IAbertura'
 
 export interface IModulo {
   id: string | number
   x: number
   y: number
-  // Aquí usamos un "Partial" o una versión simplificada
-  // para que coincida con lo que tienes en el useState del Stage
   abertura: {
     linea: string
     abertura: string
@@ -20,6 +17,5 @@ export interface IModulo {
     descripcion: string
     imgSrc: string
     variantKey: number
-    // Permitimos que tenga campos de IAbertura si existen, pero no son obligatorios
   } & Partial<IAbertura>
 }
